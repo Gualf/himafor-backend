0b0d6460df290dbda4878f1b52e318d4ad111fb0
Gualf <alfid.mufid@gmail.com>
2026-08-03 02:17:26 +0700

    CORS for Vercel fix

---
+++ src/main/java/himafor_project/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package himafor_project.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:3000", // Izin untuk testing di laptop
+                        "https://himafor-frontend.vercel.app" // Izin khusus Vercel kamu
+                        // Tambahkan link Vercel utama kamu di sini jika nanti linknya berubah
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
